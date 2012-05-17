5d65b1860d580112947cf711b1ed2063594bcf92
Flurin Egger <flurin@digitpaint.nl>
2012-05-17 14:25:30 +0200

    Bootstrap stuff

---
+++ lib/sass-fontimage.rb
@@ -0,0 +1,11 @@
+require "rubygems"
+require "bundler/setup"
+
+require 'pathname'
+require 'rmagick'
+
+# initialize module namespace
+module SassFontimage; end
+
+
+require_relative "sass_fontimage/font_image"
