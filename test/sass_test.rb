d42cbb6834500ab00f6c80336e1f91839a09385b
Flurin Egger <flurin@digitpaint.nl>
2012-07-22 12:33:47 +0200

    Add tests to work with generator plugin

--- test/sass_test.rb
+++ test/sass_test.rb
@@ -1,6 +1,7 @@
 require File.dirname(File.expand_path(__FILE__)) + "/../lib/sass-fontimage.rb"
 
 require 'sass'
+require 'sass/plugin'
 
 require File.dirname(File.expand_path(__FILE__)) + "/../lib/sass_fontimage/sass_extensions.rb"
 
@@ -14,7 +15,19 @@ $f : font_for_images("test/fonts/testfont-regular.ttf");
 }
 EOS
 
-engine = Sass::Engine.new(scss, :syntax => :scss)
-puts engine.render
+# engine = Sass::Engine.new(scss, :syntax => :scss)
+# puts engine.render
+
+Sass::Plugin.options[:style] = :expanded
+Sass::Plugin.options[:template_location] = File.dirname(__FILE__) + "/scss"
+Sass::Plugin.options[:css_location] = File.dirname(__FILE__) + "/scss"
+Sass::Plugin.options[:custom] ||= {}
+Sass::Plugin.options[:custom][:font_image_location] = File.dirname(__FILE__) + "/output"
+# Sass::Plugin.options[:custom][:font_image_url] = ""
+Sass::Plugin.options[:always_update] = true
+
+# compiler = Sass::Plugin::Compiler.new()
+
+Sass::Plugin.update_stylesheets()
 
 
